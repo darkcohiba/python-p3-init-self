56048f3f83e77a929f3a1e944d640454ec2ed136
Sam <samuel.waters@gmail.com>
2022-11-09 15:52:40 -0700

    finished

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
+# class Dog:
+#     def __init__(self, name, breed, color, owner):
+#         self.name = name
+#         self.breed = breed
+#         self.color = color
+#         self.owner = owner
+
+#     def bark(self):
+#         print("Woof!")
+    
+#     def showing_self(self):
+#         print(f"the dog breed is {self.breed}, color is {self.color}, and name is {self.name}. The owner is {self.owner}.")
+
+#     def adopt(dog, owner_name):
+#         dog.owner = owner_name
+    
+
+# leo = Dog("Leo", "German Shorthair Pointer", "Brown", "Me")
+# leo.bark()
+# leo.showing_self()
+# leo.adopt("You")
+# leo.showing_self()
+
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
